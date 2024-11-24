f0925ef4b9328a7e4496da739cfe402d01834636
Tomasz Kawalec <tkawalec369@gmail.com>
2024-11-24 18:56:53 +0100

    Remove commented out code and fix bug where gpio_ctl was not closed properly

--- src/main.c
+++ src/main.c
@@ -31,43 +31,9 @@ int main(int argc, char** argv) {
   gpio_ctl_t gpio_ctl;
   uint32_t gpio_out_lines[] = {25};
   gpio_init(&gpio_ctl, gpio_dev_path, 1, gpio_out_lines, 0, NULL);
-  /*int gpio_fd = open(gpio_dev_path, 0);
-  struct gpiochip_info gpio_cinfo;
-  IOCTL_WITH_ERR_HANDLING(gpio_fd, GPIO_GET_CHIPINFO_IOCTL, &gpio_cinfo,
-                          "couldn't open GPIO device %s", gpio_dev_path);
-  printf("GPIO %s\n  name: %s\n  label: %s\n  lines: %d\n", gpio_dev_path,
-         gpio_cinfo.name, gpio_cinfo.label, gpio_cinfo.lines);
-  for (uint32_t i = 0; i < gpio_cinfo.lines; i++) {
-    struct gpio_v2_line_info gpio_linfo;
-    memset(&gpio_linfo, 0, sizeof(gpio_linfo));
-    gpio_linfo.offset = i;
-    IOCTL_WITH_ERR_HANDLING(gpio_fd, GPIO_V2_GET_LINEINFO_IOCTL, &gpio_linfo,
-                            "couldn't get GPIO device %s line %d info\n",
-                            gpio_dev_path, i);
-    printf("  line %d:\n    name: %s\n    consumer: %s\n    num_attrs: %d\n", i,
-           gpio_linfo.name, gpio_linfo.consumer, gpio_linfo.num_attrs);
-  }
 
-  struct gpio_v2_line_request gpio_req;
-  memset(&gpio_req, 0, sizeof(gpio_req));
-  gpio_req.config.flags = GPIO_V2_LINE_FLAG_OUTPUT;
-  gpio_req.config.num_attrs = 0;
-  gpio_req.offsets[0] = 25;
-  char gpio_consumer[] = "blimp-ip-over-lora";
-  strncpy(gpio_req.consumer, gpio_consumer, GPIO_MAX_NAME_SIZE);
-  gpio_req.num_lines = 1;
-  IOCTL_WITH_ERR_HANDLING(gpio_fd, GPIO_V2_GET_LINE_IOCTL, &gpio_req,
-                          "couldn't get GPIO device %s line %d\n",
-                          gpio_dev_path, 25);*/
   for (uint8_t i = 0; i < 10; i++) {
     gpio_set(&gpio_ctl, i % 2, 0b1);
-    /*struct gpio_v2_line_values gpio_vals;
-    / *gpio_vals.bits = ((uint64_t)(i % 2)) << 25;* /
-    / *gpio_vals.mask = ((uint64_t)1) << 25;* /
-    gpio_vals.bits = (uint64_t)(i % 2);
-    gpio_vals.mask = 0b1;
-    IOCTL_WITH_ERR_HANDLING(gpio_req.fd, GPIO_V2_LINE_SET_VALUES_IOCTL,
-                            &gpio_vals, "couldn't set GPIO values\n");*/
 
     struct timespec delay_time;
     delay_time.tv_sec = 0;
@@ -75,8 +41,7 @@ int main(int argc, char** argv) {
     nanosleep(&delay_time, NULL);
   }
 
-  /*close(gpio_req.fd);
-  close(gpio_fd);*/
+  gpio_deinit(&gpio_ctl);
 
   spi_deinit(&spi);
 }
