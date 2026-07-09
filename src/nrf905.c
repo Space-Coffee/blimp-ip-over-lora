368671049b259ce7dcd44bf5c4ba3a5814dc101b
Tomasz Kawalec <tkawalec369@gmail.com>
2026-07-09 17:46:04 +0200

    spi: Add spi_transact function, for more general transactions - because nRF905 has a distinct SPI communication convention

--- src/nrf905.c
+++ src/nrf905.c
@@ -4,4 +4,7 @@
 #include "nrf905.h"
 #include "spi.h"
 
-void nrf905_init(nrf905_t* self, spi_t* spi, gpio_ctl_t* gpio) {}
+void nrf905_init(nrf905_t* self, spi_t* spi, gpio_ctl_t* gpio) {
+  self->spi = spi;
+  self->gpio = gpio;
+}
