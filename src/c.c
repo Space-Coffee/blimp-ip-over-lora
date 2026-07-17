b8b0668b933c7384755a35840caa342496899f38
Tomasz Kawalec <tkawalec369@gmail.com>
2026-07-17 15:54:33 +0200

    Translate other SPI-related functions to Zig

--- src/c.c
+++ src/c.c
@@ -1,3 +1,8 @@
 #include <fcntl.h>
 #include <linux/spi/spi.h>
 #include <linux/spi/spidev.h>
+#include <stdint.h>
+
+// Zig has issues with some more complex macros
+const uint64_t SPI_IOC_MESSAGE_1 = SPI_IOC_MESSAGE(1);
+const uint64_t SPI_IOC_MESSAGE_2 = SPI_IOC_MESSAGE(2);
--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,9 @@ const gpio = @import("gpio.zig");
 
 pub fn main(init: std.process.Init) !void {
     const spi_dev_path: [:0]const u8 = "/dev/spidev0.0";
-    const spi_dev = try spi.Spi.init(init.io, spi_dev_path);
-    _ = spi_dev;
+    var spi_dev = try spi.Spi.init(init.io, spi_dev_path);
+    defer spi_dev.deinit(init.io);
+    try spi_dev.write_reg8(0b10110001, 0x00);
 
     const gpio_dev_path: [:0]const u8 = "/dev/gpiochip0";
     const gpio_ctrl = try gpio.GpioCtrl.init(init.io, gpio_dev_path);
